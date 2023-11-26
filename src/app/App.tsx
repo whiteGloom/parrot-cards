5349dce0fc1deca0c989e0d4354c16f0f0dc697d
emiller <karabin.m@yandex.ru>
2023-11-26 10:21:02 +0600

    [main] Project rearrange

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.scss';
+import './styles/index.scss';
 
 const GOOGLE_OAUTH_KEY = process.env.REACT_APP_GOOGLE_OAUTH_KEY as string;
 const GOOGLE_DRIVE_API_KEY = process.env.REACT_APP_GOOGLE_DRIVE_API_KEY as string;
@@ -27,7 +27,6 @@ function App() {
           .then(
             () => {
               setClientInited(true);
-              console.log('wgl inited');
             },
             (error) => {
               console.log('Initialization of clint of Google API failed:', error);
@@ -40,9 +39,8 @@ function App() {
         scope: 'https://www.googleapis.com/auth/drive.metadata.readonly',
         callback: (res) => {
           gapi.client.drive.files.list({
-            // @ts-expect-error test
-            'pageSize': 10,
-            'fields': 'files(id, name)',
+            // @ts-expect-error TODO: make a pr to DefinitelyTyped
+            pageSize: 10,
           }).then(
             (res) => {
               console.log('List files response', res);
@@ -60,6 +58,41 @@ function App() {
 
   return (
     <div className="App">
+
+      <button
+        onClick={() => {
+          const oauth2Endpoint = 'https://accounts.google.com/o/oauth2/v2/auth';
+
+          // Create <form> element to submit parameters to OAuth 2.0 endpoint.
+          const form = document.createElement('form');
+          form.setAttribute('method', 'GET'); // Send as a GET request.
+          form.setAttribute('action', oauth2Endpoint);
+
+          // Parameters to pass to OAuth 2.0 endpoint.
+          const params = {
+            client_id: GOOGLE_OAUTH_KEY,
+            redirect_uri: 'http://localhost:3000',
+            response_type: 'token',
+            scope: 'https://www.googleapis.com/auth/drive.metadata.readonly',
+            include_granted_scopes: 'true',
+            state: 'pass-through value',
+          };
+
+          // Add form parameters as hidden input values.
+          for (const p in params) {
+            const input = document.createElement('input');
+            input.setAttribute('type', 'hidden');
+            input.setAttribute('name', p);
+            input.setAttribute('value', params[p as keyof typeof params]);
+            form.appendChild(input);
+          }
+
+          // Add form to page and submit it to open the OAuth 2.0 endpoint.
+          document.body.appendChild(form);
+          form.submit();
+        }}
+      >Hello</button>
+
       {tokenClient && isClientInited ? (
         <button
           onClick={() => {
