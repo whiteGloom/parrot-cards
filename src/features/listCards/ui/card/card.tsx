28ebdddf7e4ab64846ee4eebd4c99ca5da91404b
emiller <karabin.m@yandex.ru>
2023-11-26 23:32:10 +0600

    [main] Temp changes at card
    Changelog:
    * Hints added

--- src/features/listCards/ui/card/card.tsx
+++ src/features/listCards/ui/card/card.tsx
@@ -1,28 +1,34 @@
 import React, {FC} from 'react';
 import styles from './styles.module.scss';
 import {ICard} from '../../../../entity/card';
+import {Hint} from '../hint/hint';
 
 export interface CardProps {
-  cardModel: ICard;
+  cardData: ICard;
 }
 
 export const Card: FC<CardProps> = (props) => {
   const [flip, setFlip] = React.useState(false);
 
-  const currentSideData = flip ? props.cardModel.sideTwo : props.cardModel.sideOne;
+  const currentSideData = flip ? props.cardData.backSide : props.cardData.frontSide;
 
   return (
     <div className={styles.card} onClick={() => {setFlip((flip) => !flip);}}>
-      <p>{flip ? 'Back side' : 'Front side'}</p>
+      <p>{flip ? 'BACK SIDE' : 'FRONT SIDE'}</p>
       <p>Title: {currentSideData.title}</p>
 
       {currentSideData.description.length ? (
         <p className={styles.description}>Description: {currentSideData.description}</p>
       ) : undefined}
 
-      {currentSideData.hints.length ? (
-        <p>Hints: {currentSideData.hints.join(', ')}</p>
-      ) : undefined}
+      {currentSideData.hints.length ? currentSideData.hints.map((hint) => {
+        return (
+          <div style={{display: 'flex'}} key={hint}>
+            {'Hint: '}
+            <Hint title={hint}/>
+          </div>
+        );
+      }) : undefined}
     </div>
   );
 };
