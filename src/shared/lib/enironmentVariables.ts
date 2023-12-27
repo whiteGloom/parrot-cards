63728a47e174594b6b5a4caefae46fd604c3852c
emiller <karabin.m@yandex.ru>
2023-12-28 00:14:44 +0600

    [main] environment lib added

---
+++ src/shared/lib/enironmentVariables.ts
@@ -0,0 +1,5 @@
+export const ENV_GOOGLE_OAUTH_KEY = process.env.REACT_APP_GOOGLE_OAUTH_KEY as string;
+
+export const ENV_GOOGLE_DRIVE_API_KEY = process.env.REACT_APP_GOOGLE_DRIVE_API_KEY as string;
+
+export const ENV_GOOGLE_TEMP_OAUTH_TOKEN = process.env.REACT_APP_GOOGLE_TEMP_AUTH_TOKEN as string;
