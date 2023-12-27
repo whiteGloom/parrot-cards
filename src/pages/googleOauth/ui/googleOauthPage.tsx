bdff0598b7831bc2b4eb8c36472c043a923c08de
emiller <karabin.m@yandex.ru>
2023-12-28 00:19:16 +0600

    [main] oauth page temp update

--- src/pages/googleOauth/ui/googleOauthPage.tsx
+++ src/pages/googleOauth/ui/googleOauthPage.tsx
@@ -1,5 +1,7 @@
 import React, {FC} from 'react';
-import {useNavigate} from 'react-router-dom';
+import {Link} from 'react-router-dom';
+import {OauthLoginButton} from '../../../features/google/oauthLogin';
+import {OpenType} from '../../../features/google/oauthLogin/api/openOauthPageThunk';
 
 type ExpectedSuccessParamsFromAuth = {
   access_token: string,
@@ -11,6 +13,11 @@ type ExpectedSuccessParamsFromAuth = {
 
 type ExpectedErrorParamsFromAuth = {
   error: string,
+  state?: string,
+}
+
+type StateExpectedParams = {
+  scopes?: string[],
 }
 
 function isSuccessParams(params: Record<string, string>): params is ExpectedSuccessParamsFromAuth {
@@ -18,41 +25,84 @@ function isSuccessParams(params: Record<string, string>): params is ExpectedSucc
     'access_token',
     'expires_in',
     'scope',
-    'state',
     'token_type',
   ];
 
-  return Object.keys(params).every((key) => expectedKeys.includes(key as keyof ExpectedSuccessParamsFromAuth));
+  const paramsKeys = Object.keys(params);
+
+  return expectedKeys.every((key) => paramsKeys.includes(key));
 }
 
 function isErrorParams(params: Record<string, string>): params is ExpectedErrorParamsFromAuth {
   const expectedKeys: (keyof ExpectedErrorParamsFromAuth)[] = ['error'];
+  const paramsKeys = Object.keys(params);
 
-  return Object.keys(params).every((key) => expectedKeys.includes(key as keyof ExpectedErrorParamsFromAuth));
+  return expectedKeys.every((key) => paramsKeys.includes(key));
+}
+
+function parseState(stateString: string): StateExpectedParams {
+  try {
+    return JSON.parse(decodeURI(stateString)) as StateExpectedParams;
+  } catch (err) {
+    return {};
+  }
+}
+
+function SuccessView() {
+  return (
+    <div>
+      <p>You successfully authenticated. You can close this page or continue here</p>
+      <Link to={'/'}>Go home</Link>
+    </div>
+  );
+}
+
+function ErrorView(props: {params: ExpectedErrorParamsFromAuth}) {
+  const state = parseState(props.params.state || '');
+
+  return (
+    <div>
+      <p>Authentication failed with error.</p>
+
+      {state.scopes
+        ? <p>You can close this page, retry authentication or go home</p>
+        : <p>You can close this page or go home</p>
+      }
+
+      <p>The error: {props.params.error}</p>
+
+      <Link to={'/'}>Go home</Link>
+      {state.scopes ? (
+        <OauthLoginButton scopes={state.scopes} openType={OpenType.INLINE}>Retry</OauthLoginButton>
+      ) : undefined}
+    </div>
+  );
 }
 
 export const GoogleOauthPage: FC = () => {
-  const navigate = useNavigate();
+  const params = React.useMemo(() => {
+    return window.location.hash
+      .slice(1).split('&')
+      .reduce((reducer, paramsString) => {
+        const [key, value] = paramsString.split('=');
+        reducer[key] = value;
 
-  React.useEffect(() => {
-    const params = window.location.hash.slice(1).split('&').reduce((reducer, paramsString) => {
-      const [key, value] = paramsString.split('=');
-      reducer[key] = value;
-      return reducer;
-    }, {} as Record<string, string>);
+        return reducer;
+      }, {} as Record<string, string>);
+  }, []);
 
-    if (isSuccessParams(params)) {
-      console.log('wgl GoogleOauthPage.success params', params, `Authorization=${params.token_type} ${params.access_token}; Max-Age=${params.expires_in}; SameSite=None; Secure`);
-    }
+  if (isSuccessParams(params)) {
+    return <SuccessView/>;
+  }
 
-    if (isErrorParams(params)) {
-      console.log('Authorization error', params.error);
-    }
-
-    navigate('/', {replace: true});
-  }, [navigate]);
+  if (isErrorParams(params)) {
+    return <ErrorView params={params}/>;
+  }
 
   return (
-    <div>Loading...</div>
+    <div>
+      <p>Wrong URL of the page.</p>
+      <Link to={'/'}>Go home</Link>
+    </div>
   );
 };
