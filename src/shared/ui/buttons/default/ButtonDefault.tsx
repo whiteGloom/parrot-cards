0808adc380b413240cb12f2880a46faea099ee00
emiller <karabin.m@yandex.ru>
2024-01-17 00:47:15 +0600

    [main] button component added

---
+++ src/shared/ui/buttons/default/ButtonDefault.tsx
@@ -0,0 +1,37 @@
+import React, {FC, PropsWithChildren, ButtonHTMLAttributes, DetailedHTMLProps} from 'react';
+import clsx from 'clsx';
+
+export enum ButtonDefaultTypes {
+  Normal = 1,
+  Accent,
+  Warning
+}
+
+export type ButtonDefaultPropsType =
+  {
+    theme?: ButtonDefaultTypes,
+  }
+  & DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>
+  & PropsWithChildren;
+
+export const ButtonDefault: FC<ButtonDefaultPropsType> = (props) => {
+  const {
+    theme = ButtonDefaultTypes.Normal,
+    className,
+    children,
+    ...otherProps} = props;
+
+  return (
+    <button
+      className={clsx([
+        'rounded px-2 py-1',
+        theme === ButtonDefaultTypes.Normal && 'font-semibold shadow border text-[#2b2b2b] bg-white border-gray-200 hover:bg-[#f2f2f2] active:bg-[#E7E7E7]',
+        theme === ButtonDefaultTypes.Warning && 'font-semibold shadow text-white bg-rose-600 hover:bg-rose-700 active:bg-rose-800',
+        className,
+      ])}
+      {...otherProps}
+    >
+      {children}
+    </button>
+  );
+};
