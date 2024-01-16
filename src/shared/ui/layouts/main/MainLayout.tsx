eec5c5446ef9a8c23d8eb0375e9bec16a0317e6f
emiller <karabin.m@yandex.ru>
2024-01-16 22:33:31 +0600

    [main] main layout component added

---
+++ src/shared/ui/layouts/main/MainLayout.tsx
@@ -0,0 +1,11 @@
+import React, {FC, PropsWithChildren} from 'react';
+
+export const MainLayout: FC<PropsWithChildren> = (props) => {
+  return (
+    <div className={'flex w-full h-full'}>
+      <div className={'container mx-auto flex flex-col gap-5 p-5 lg:max-w-screen-lg'}>
+        {props.children}
+      </div>
+    </div>
+  );
+};
