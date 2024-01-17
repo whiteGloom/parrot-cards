ff0fd04d912176914c587d80ef4b9802edda4f10
emiller <karabin.m@yandex.ru>
2024-01-17 21:13:54 +0600

    [main] layout updated

--- src/shared/ui/layouts/main/MainLayout.tsx
+++ src/shared/ui/layouts/main/MainLayout.tsx
@@ -2,7 +2,7 @@ import React, {FC, PropsWithChildren} from 'react';
 
 export const MainLayout: FC<PropsWithChildren> = (props) => {
   return (
-    <div className={'flex w-full h-full'}>
+    <div className={'flex w-full h-full overflow-auto'}>
       <div className={'container mx-auto flex flex-col gap-5 p-5 lg:max-w-screen-lg'}>
         {props.children}
       </div>
