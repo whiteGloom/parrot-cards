0b3b33d20543fd7c8b879f9c6f9bd373de359301
emiller <karabin.m@yandex.ru>
2024-01-19 18:53:43 +0600

    [main] next card on revise page fixed

--- src/pages/revise/ui/Revise/Revise.tsx
+++ src/pages/revise/ui/Revise/Revise.tsx
@@ -1,5 +1,5 @@
 import React, {FC} from 'react';
-import {useLocation, useParams, useSearchParams} from 'react-router-dom';
+import {Navigate, useLocation, useParams, useSearchParams} from 'react-router-dom';
 import {Card} from '../Card/Card';
 import {useSelector} from 'react-redux';
 import {selectCardsIdsByFilters} from '../../model/selectors/selectCardsIdsByFilters';
@@ -14,6 +14,14 @@ export const Revise: FC = () => {
   const location = useLocation();
 
   const cardId = useParams().cardId;
+  const cardIndex = cardId ? cardsIds.indexOf(cardId) : -1;
+  const nextCardIndex = cardIndex + 1 < cardsIds.length ? cardIndex + 1 : 0;
+
+  if (!cardId) {
+    return (
+      <Navigate to={'/'}/>
+    );
+  }
 
   return (
     <MainLayout>
@@ -24,16 +32,16 @@ export const Revise: FC = () => {
 
       <section className={'flex flex-col p-3 gap-3 border rounded bg-gray-50'}>
         <div className={'flex flex-col gap-3 items-center'}>
-          {cardId ? (
-            <Card cardId={cardId} key={cardId}/>
-          ) : 'Card not found'}
-
           <LinkButton
-            to={`/revise/${cardsIds[Math.round(Math.random() * (cardsIds.length - 1))]}${location.search}`}
+            to={`/revise/${cardsIds[nextCardIndex]}${location.search}`}
             theme={LinkButtonDefaultTypes.Accent}
           >
             Next Card
           </LinkButton>
+
+          {cardsIds.includes(cardId) ? (
+            <Card cardId={cardId} key={cardId}/>
+          ) : <div className={'bg-white p-3 border shadow rounded font-bold'}>Card no found!</div>}
         </div>
       </section>
     </MainLayout>
