e7eb2db491f8fbac79a205c600067a5ad4ad89d2
emiller <karabin.m@yandex.ru>
2024-01-19 19:39:02 +0600

    [main] card click zone updated

--- src/pages/revise/ui/Card/Card.tsx
+++ src/pages/revise/ui/Card/Card.tsx
@@ -19,16 +19,18 @@ export const Card: FC<CardPropsType> = (props) => {
   const currentSide = isFrontSideVisible ? cardData.frontSide : cardData.backSide;
 
   return (
-    <article
-      className={'flex flex-col gap-3 p-3 items-center bg-white border rounded shadow min-w-[500px]'}
-      onClick={() => {setFrontSideVisible((value) => !value);}}
-    >
-      <p className={'text-gray-800 uppercase'}>{isFrontSideVisible ? 'Front side' : 'Back side'}</p>
-      <p className={'text-3xl cursor-pointer'}>{currentSide.title}</p>
+    <article className={'flex flex-col gap-3 p-3 items-center bg-white border rounded shadow min-w-[500px]'}>
+      <div
+        className={'flex flex-col gap-3 items-center w-full'}
+        onClick={() => {setFrontSideVisible((value) => !value);}}
+      >
+        <p className={'text-gray-800 uppercase'}>{isFrontSideVisible ? 'Front side' : 'Back side'}</p>
+        <p className={'text-3xl cursor-pointer'}>{currentSide.title}</p>
 
-      {currentSide.description.length ? (
-        <p>{currentSide.description}</p>
-      ) : undefined}
+        {currentSide.description.length ? (
+          <p>{currentSide.description}</p>
+        ) : undefined}
+      </div>
 
       <ul className={'flex flex-col gap-2 w-full'}>
         {currentSide.hints.length ? currentSide.hints.map((hint) => (
