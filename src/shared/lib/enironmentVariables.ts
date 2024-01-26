12754e869f6c035933191c94bc47ce1cc3197f46
emiller <karabin.m@yandex.ru>
2024-01-27 00:52:44 +0600

    [main] old env property removed

--- src/shared/lib/enironmentVariables.ts
+++ src/shared/lib/enironmentVariables.ts
@@ -1,5 +1,3 @@
 export const ENV_GOOGLE_OAUTH_KEY = process.env.REACT_APP_GOOGLE_OAUTH_KEY as string;
 
 export const ENV_GOOGLE_DRIVE_API_KEY = process.env.REACT_APP_GOOGLE_DRIVE_API_KEY as string;
-
-export const ENV_GOOGLE_TEMP_OAUTH_TOKEN = process.env.REACT_APP_GOOGLE_TEMP_AUTH_TOKEN as string;
