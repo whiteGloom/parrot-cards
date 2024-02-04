2233a54faae39773fc9fd0ea34c4d1ab4d0419ac
emiller <karabin.m@yandex.ru>
2024-02-05 00:43:18 +0600

    [main] library for short uids added

--- src/shared/lib/UniqueIdGenerator/UniqueIdGenerator.tsx
+++ src/shared/lib/UniqueIdGenerator/UniqueIdGenerator.tsx
@@ -1,7 +1,12 @@
 import {v4 as UUIDGenerator} from 'uuid';
+import {uid} from 'uid';
 
 export class UniqueIdGenerator {
   public static generateSimpleUniqueId(): string {
     return UUIDGenerator();
+  }
+
+  public static generateUID(length = 16): string {
+    return uid(length);
   }
 }
