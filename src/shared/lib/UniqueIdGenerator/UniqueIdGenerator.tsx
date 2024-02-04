52ac362c8699f524c5ca3c5e6064af5d915bd80b
emiller <karabin.m@yandex.ru>
2024-02-05 01:03:04 +0600

    [main] id generation fixed
    Lost changes for: acd7b370

--- src/shared/lib/UniqueIdGenerator/UniqueIdGenerator.tsx
+++ src/shared/lib/UniqueIdGenerator/UniqueIdGenerator.tsx
@@ -2,7 +2,7 @@ import {v4 as UUIDGenerator} from 'uuid';
 import {uid} from 'uid';
 
 export class UniqueIdGenerator {
-  public static generateSimpleUniqueId(): string {
+  public static generateUUID(): string {
     return UUIDGenerator();
   }
 
