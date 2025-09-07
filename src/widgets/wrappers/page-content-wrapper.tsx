0dcbf9b9ced1e566ffdb3f494ae09d62b089dbba
whiteGloom <karabin.m@yandex.ru>
2025-09-07 23:47:31 +0600

    feat: mobile layout fixed
    BranchName: main

--- src/widgets/wrappers/page-content-wrapper.tsx
+++ src/widgets/wrappers/page-content-wrapper.tsx
@@ -5,11 +5,11 @@ export function PageContentWrapper(props: PropsWithChildren<{ contentWrapperClas
   return (
     <div className="min-h-full p-3 bg-gradient-to-tr from-purple-300 to-blue-300 overflow-auto">
       <div
-        className="grid min-h-full min-w-3xl place-content-center"
+        className="grid min-h-full md:place-content-center content-center"
       >
         <div
           className={clsx([
-            'flex flex-col gap-4 md:min-w-3xl min-w-full',
+            'flex flex-col gap-4 md:min-w-3xl',
             props.contentWrapperClassName,
           ])}
         >
