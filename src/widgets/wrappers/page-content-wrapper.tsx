77b5aa2b25ebec91453905c2e6a74f811c0b87ad
whiteGloom <karabin.m@yandex.ru>
2025-09-07 15:57:09 +0600

    feat: content scrolling fixed
    BranchName: 2.0

--- src/widgets/wrappers/page-content-wrapper.tsx
+++ src/widgets/wrappers/page-content-wrapper.tsx
@@ -1,18 +1,20 @@
-import type { PropsWithChildren } from 'react';
+import { type PropsWithChildren } from 'react';
 import clsx from 'clsx';
 
 export function PageContentWrapper(props: PropsWithChildren<{ contentWrapperClassName?: string }>) {
   return (
-    <div
-      className="flex flex-col min-h-full min-w-3xl justify-center items-center bg-gradient-to-tr from-purple-300 to-blue-300 p-3"
-    >
+    <div className="min-h-full p-3 bg-gradient-to-tr from-purple-300 to-blue-300 overflow-auto">
       <div
-        className={clsx([
-          'flex flex-col gap-4 md:min-w-3xl min-w-full',
-          props.contentWrapperClassName,
-        ])}
+        className="grid min-h-full min-w-3xl place-content-center"
       >
-        {props.children}
+        <div
+          className={clsx([
+            'flex flex-col gap-4 md:min-w-3xl min-w-full',
+            props.contentWrapperClassName,
+          ])}
+        >
+          {props.children}
+        </div>
       </div>
     </div>
   );
