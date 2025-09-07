9a7641c2c8b7094271fdb829bf5f06941bef6cb7
whiteGloom <karabin.m@yandex.ru>
2025-09-07 18:31:19 +0600

    Updates

---
+++ assets/inline-for-D-FZR1qe.js
@@ -0,0 +1,1 @@
+function i(t,e){const r=[];for(let n=0;n<t;n++)r.push(e(n));return r}export{i};
---
+++ assets/input-wrapped-B9CbG3P0.js
@@ -0,0 +1,16 @@
+import{c as t}from"./createLucideIcon-PNBb2RY-.js";import{j as a}from"./index-C-Rh5RM2.js";/**
+ * @license lucide-react v0.539.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const r=[["path",{d:"M12 3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7",key:"1m0v6g"}],["path",{d:"M18.375 2.625a1 1 0 0 1 3 3l-9.013 9.014a2 2 0 0 1-.853.505l-2.873.84a.5.5 0 0 1-.62-.62l.84-2.873a2 2 0 0 1 .506-.852z",key:"ohrbg2"}]],h=t("square-pen",r);/**
+ * @license lucide-react v0.539.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const d=[["path",{d:"M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6",key:"miytrc"}],["path",{d:"M3 6h18",key:"d0wm0j"}],["path",{d:"M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2",key:"e791ji"}]],o=t("trash",d);/**
+ * @license lucide-react v0.539.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const c=[["path",{d:"M18 6 6 18",key:"1bl5f8"}],["path",{d:"m6 6 12 12",key:"d8bk6v"}]],u=t("x",c);function l(e){return a.jsxs("label",{className:"flex flex-col gap-1",htmlFor:e.id,children:[a.jsxs("p",{children:[e.label+" ",e.isRequired&&a.jsx("span",{className:"text-red-600",children:"*"})]}),e.children,!!e.error&&a.jsx("p",{className:"text-red-600",children:e.error})]})}function m(e){return a.jsx(l,{label:e.label,isRequired:e.isRequired,id:e.name,error:e.error,children:a.jsx("input",{onChange:n=>{e.onChange(n)},autoFocus:e.autofocus||!1,name:e.name,id:e.name,value:e.value,type:e.type||"text",className:"border p-1 rounded border-gray-400 shadow bg-white"})})}export{m as I,h as S,o as T,u as X,l as a};
